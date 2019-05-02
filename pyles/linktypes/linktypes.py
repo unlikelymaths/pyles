2661809bef7b1056e40877e950a5335248964f1b
Markus Plack <markus.plack@gmail.com>
2019-05-02 17:28:48 +0200

    Fix import path for linktype plugins

--- pyles/linktypes/linktypes.py
+++ pyles/linktypes/linktypes.py
@@ -6,10 +6,9 @@ import linktypes.default_linktype as default
 all = {mod.name: mod for mod in [default] + [
     importlib.import_module('linktypes.{}'.format(name))
     for finder, name, ispkg
-    in pkgutil.iter_modules(path=['linktypes'])
+    in pkgutil.iter_modules(path=['pyles\linktypes'])
     if name.startswith('pylesltype_')
     ]}
-
 
 for t in all.keys():
     print(t)
