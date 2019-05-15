9ffe50ee5023f82f3cbccc13cba96e29afda52c5
Markus Plack <markus.plack@gmail.com>
2019-05-15 15:20:22 +0200

    Move pyles app to separate file

---
+++ pyles/app.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+
+from widgets.common import DropManager
+from widgets.pyles_list import PylesList
+from widgets.pyles_new import PylesNew
+from widgets.util import widget_path
+
+class Pyles(App,DropManager):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def on_start(self):
+        self.set_widget('list')
+
+    def set_widget(self, widget_name):
+        self.root.clear_widgets()
+        if widget_name == 'list':
+            self.root.add_widget(PylesList())
+        elif widget_name == 'new':
+            self.root.add_widget(PylesNew())
